f06d3399dac8b80ffd2e8d03fec99d2224386435
Claes Lindvall <claes.lindvall@intraphone.com>
2023-10-31 12:07:54 +0100

    laksdj

--- cl_clean_proj_and_images.sh
+++ cl_clean_proj_and_images.sh
@@ -6,4 +6,4 @@ echo "Removing all containers"
 podman rm $(podman ps -a | grep -v CONTAINER | awk '{ print $1 '})
 
 echo "Cleaning up all images"
-podman rmi $(podman images | grep -v IMAGE | awk '{ print $3 }')
+podman rmi $(podman images | grep -v IMAGE | awk '{ print $3 }') --force
